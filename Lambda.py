efc199ebf136308281fa773f9a35fd0342b87fe0
jason <jbritchkow@crimson.ua.edu>
2017-04-19 17:36:16 -0500

    updated gitignore

--- Lambda.py
+++ Lambda.py
@@ -233,9 +233,6 @@ def on_session_ended(session_ended_request, session, mongo_client):
 
 
 # --------------- Main handler ------------------
-def test_func():
-    print('test')
-
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
