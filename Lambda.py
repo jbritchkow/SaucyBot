fdb7352701903ea746e596bc5b07c855ec17b37a
jason <jbritchkow@crimson.ua.edu>
2017-04-23 23:07:05 -0500

    moved around mongo client stuff so it is just in lambda handler

--- Lambda.py
+++ Lambda.py
@@ -478,8 +478,6 @@ def on_session_started(session_started_request, session):
 
     print("on_session_started requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
-    client = pymongo.MongoClient(uri)
-    return client
 
 
 def on_launch(launch_request, session):
@@ -537,14 +535,13 @@ def on_intent(intent_request, session):
         raise ValueError("Invalid intent")
 
 
-def on_session_ended(session_ended_request, session, m_client):
+def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
 
     Is not called when the skill returns should_end_session=true
     """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    m_client.close()
 
 
 # --------------- Main handler ------------------
@@ -568,13 +565,16 @@ def lambda_handler(event, context):
 
     if event['session']['new']:
         mongo_client = pymongo.MongoClient(uri)
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
 
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'], mongo_client)
+        mongo_client.close()
+        return on_session_ended(event['request'], event['session'])
 
 
 # ------------------ Database Methods --------------------------
